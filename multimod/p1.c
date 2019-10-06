78fa5cd62fde9a9904ddb00a1b3efe99b8c1578b
bright <1019392083@qq.com>
2019-10-06 15:01:09 +0800

    first draft of p1

--- multimod/p1.c
+++ multimod/p1.c
@@ -2,5 +2,23 @@
 
 int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
-  return -1;
+  int sign = 0;
+  if(a==0 || b==0) return 0;
+  if(a<0) a=-a;
+  if(b<0) b=-b;
+  int sum = 0;
+  int ad = 0;
+  int tmp = 1;
+  int rs1 = b%m;
+  for(int i=0; i<64; i++){
+    ad = 1&a ;
+	a >> 1;
+	tmp << i;
+	tmp *= ad;
+	tmp = tmp%m;
+	sum += rs1 * tmp;
+	tmp = 1;
+  } 
+  sum = sum%m;
+  return sum;
 }
