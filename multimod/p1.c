24a69678834d8c28c71a2a39fc9446e5504a412a
tracer-nju <tracer@nju.edu.cn>
2019-10-06 15:34:50 +0800

    Static hostname: Debian10          Icon name: computer-vm            Chassis: vm         Machine ID: c086f65718d8d14cb267055aef74405c            Boot ID: d9b4fd5c51eb4c458caef181363ebd35     Virtualization: kvm   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-5-amd64       Architecture: x86-64  15:34:50 up 5 min,  1 user,  load average: 0.29, 0.19, 0.09

--- multimod/p1.c
+++ multimod/p1.c
@@ -4,21 +4,28 @@ int64_t multimod_p1(int64_t a, int64_t b, int64_t m) {
   // TODO: implement
   int sign = 0;
   if(a==0 || b==0) return 0;
-  if(a<0) a=-a;
-  if(b<0) b=-b;
+  if(a<0){
+    sign++;
+	a=-a;
+  }
+  if(b<0){
+    sign++;
+	b=-b;
+  }
   int sum = 0;
   int ad = 0;
   int tmp = 1;
   int rs1 = b%m;
   for(int i=0; i<64; i++){
     ad = 1&a ;
-	a >> 1;
-	tmp << i;
+	a = a >> 1;
+	tmp = tmp << i;
 	tmp *= ad;
 	tmp = tmp%m;
 	sum += rs1 * tmp;
 	tmp = 1;
-  } 
-  sum = sum%m;
+  }
+  if(sign==1) sum = m-sum%m; 
+  else sum = sum%m;
   return sum;
 }
